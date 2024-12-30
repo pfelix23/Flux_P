4b8181e459293f0e6c40a4e3a8f59a722591caf1
fishstiicks <loketteshimmer@gmail.com>
2024-12-30 12:29:08 -0600

    add modal button to landing

--- react-vite/src/components/Landing_Page/LandingPage.jsx
+++ react-vite/src/components/Landing_Page/LandingPage.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 import './LandingPage.css';
+import OpenModalMenuItem from '../Navigation/OpenModalMenuItem';
+
+
 function LandingPage() {
     const [posts, setPosts] = useState([]);
     const [errors, setErrors] = useState();
     const navigate = useNavigate();
+
     useEffect(() => {
         fetch('/api/posts')
         .then((res) => res.json())
@@ -17,24 +21,35 @@ function LandingPage() {
             }
         })
     }, []);
+
     return (
         <div className='posts_section'>
             <section className='posts_section'>
                 {[...posts].reverse().map((post) => {
                     return (
-                        <picture onClick={() => navigate(`/api/posts/${post.id}`)} key={post.id}>
+                        <picture onClick={() => navigate(`/posts/${post.id}`)} key={post.id}>
                             <img src={post.image} alt={post.description} className='posts_img' />
                             <div className='added_info'>
                             <div>{post.description}</div>
                             <div>{post.likes}</div>
                             <div>{post.comment_count}</div>
+                            <OpenModalMenuItem
+                                itemText="Comments"
+                                onItemClick={closeMenu}
+                                modalComponent={<CommentsModal/>}
+                            />
                             </div>
                         </picture>
                     );
                 })
+
               }
+
             </section>
+
         </div>
     )
+
 }
+
 export default LandingPage
